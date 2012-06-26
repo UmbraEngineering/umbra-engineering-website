85728741d5b493732489121fd0ac83ce68b45f55
James Brumond <kbjr14@gmail.com>
2012-06-26 03:39:54 -0700

    no longer using hook.io

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,6 @@
 
 var consts     = require('consts');
 var app        = require(consts.LIB_PATH + '/app');
-var hook       = require(consts.LIB_PATH + '/web').hook;
-var conf       = require(consts.LIB_PATH + '/conf')('pass-reset');
 
 app.get('/foo', function(req, res) {
 	res.send('FOO!');
